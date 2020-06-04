c60366e75f7ac599e6e912c4afffb03f1401144a
liuxiaobo <p_lxboliu@tencent.com>
2020-06-04 10:48:07 +0800

    add chap2/simple spring mvc

--- springInAction/chap3/taco-jdbc/src/main/java/tacos/TacoCloudApplication.java
+++ springInAction/chap3/taco-jdbc/src/main/java/tacos/TacoCloudApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TacoCloudApplication {
+public class TacoJdbcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TacoCloudApplication.class, args);
+		SpringApplication.run(TacoJdbcApplication.class, args);
 	}
 
 }
