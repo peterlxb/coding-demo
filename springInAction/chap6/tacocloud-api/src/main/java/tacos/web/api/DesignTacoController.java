fd13e83e6c60e54f23e18e22b0ebd1dcd7536dbb
liuxiaobo <p_lxboliu@tencent.com>
2020-06-10 12:34:37 +0800

    需要将配置设为jdk-8版本

---
+++ springInAction/chap6/tacocloud-api/src/main/java/tacos/web/api/DesignTacoController.java
@@ -0,0 +1,37 @@
+package tacos.web.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tacos.Taco;
+import tacos.data.TacoRepository;
+
+@RestController
+@RequestMapping(path="/design",
+                produces="application/json")
+@CrossOrigin(origins="*")
+public class DesignTacoController {
+
+    private TacoRepository tacoRepo;
+
+    @Autowired
+    EntityLinks entityLinks;
+
+    public DesignTacoController(TacoRepository tacoRepo) {
+        this.tacoRepo = tacoRepo;
+    }
+
+    @GetMapping("/recent")
+    public Iterable<Taco> recentTacos() {
+        PageRequest page = PageRequest.of(
+                0, 12, Sort.by("createdAt").descending());
+
+        return tacoRepo.findAll(page).getContent();
+    }
+
+}
