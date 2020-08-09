e569fea448e1dd0dc5eed3efcee596478b8eb651
刘晓波 <peterlxb@gmail.com>
2020-08-09 13:39:38 +0800

    add mongo

---
+++ spring-nosql/mongo-repository-demo/src/main/java/geektime/spring/data/mongodemo/repository/CoffeeRepository.java
@@ -0,0 +1,10 @@
+package com.spring.data.mongodemo.repository;
+
+import com.spring.data.mongodemo.model.Coffee;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface CoffeeRepository extends MongoRepository<Coffee, String> {
+    List<Coffee> findByName(String name);
+}
