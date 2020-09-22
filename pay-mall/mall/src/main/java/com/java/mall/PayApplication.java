e66d34fe0625cc7df70ee6b7fc4debfef6a1c3f1
liuxiaobo <p_lxboliu@tencent.com>
2020-09-22 12:08:11 +0800

    add runner

--- pay-mall/mall/src/main/java/com/java/mall/PayApplication.java
+++ pay-mall/mall/src/main/java/com/java/mall/PayApplication.java
@@ -1,15 +1,31 @@
 package com.java.mall;
 
+import com.java.mall.dao.CategoryMapper;
+import com.java.mall.pojo.Category;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 @MapperScan(basePackages = "com.java.mall.dao")
-public class PayApplication {
+public class PayApplication implements ApplicationRunner {
+
+    @Autowired
+    private CategoryMapper categoryMapper;
 
     public static void main(String[] args) {
         SpringApplication.run(PayApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        Category category = categoryMapper.findById(100002);
+        log.info("Find category: {}",category);
     }
 
 }
