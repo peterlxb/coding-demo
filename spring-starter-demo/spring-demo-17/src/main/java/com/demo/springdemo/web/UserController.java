11ae227f97dd4d8e6b4f66944fd24358b645d59a
刘晓波 <peterlxb@gmail.com>
2021-01-20 00:21:47 +0800

    PathVariable

--- spring-starter-demo/spring-demo-17/src/main/java/com/demo/springdemo/web/UserController.java
+++ spring-starter-demo/spring-demo-17/src/main/java/com/demo/springdemo/web/UserController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -40,6 +41,15 @@ public class UserController {
     public String register(@ModelAttribute("user") User user) {
         log.info("register user");
         return "user/register";
+    }
+
+    @RequestMapping("/{userId}")
+    public ModelAndView showDetail(@PathVariable("userId") String userId) {
+        ModelAndView mav = new ModelAndView();
+
+        mav.setViewName("user/showDetail");
+        mav.addObject("user", userService.getUserById(userId));
+        return mav;
     }
 
 }
